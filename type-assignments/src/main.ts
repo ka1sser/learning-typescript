4d1a7db4a8458b0a188476e1467b395d003490e6
ka1sser <serenokaisser.dev@gmail.com>
2025-10-01 22:48:28 +0800

    object basics and optional properties

--- type-assignments/src/main.ts
+++ type-assignments/src/main.ts
@@ -1,6 +1,20 @@
-let a: any = 4
+const person = {
+    name: "Kai",
+    age: 28
+}
 
-a = 3
-a = "asdf"
+// person.isProgrammer = true // this won't work bec it is not defined
 
-// DO NOT USE ANY
+const person: { name: string; age: number, isProgrammer: boolean } = {
+    name: "Kai",
+    age: 28
+}
+
+// ^ This is now throwing an error bec isProgammer is not defined
+
+const person: { name: string; age: number, isProgrammer?: boolean } = {
+    name: "Kai",
+    age: 28
+}
+
+// add "?" at the end of the property to tell TS it is only optional
