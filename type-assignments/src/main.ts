3072deae4cd66215b72183eb4ff00f99b60d4db9
ka1sser <serenokaisser.dev@gmail.com>
2025-10-04 00:24:13 +0800

    optional parameter using ?

--- type-assignments/src/main.ts
+++ type-assignments/src/main.ts
@@ -1,19 +1,5 @@
-function printName(name: string, name2:string) {
-    console.log(name, name2)
+function printNameAndAge(name: string, age?: number) {
+    console.log(name, age)
 }
 
-function sum(a: number, b: number): number {
-    return a+b
-}
-
-const c = sum(1, 2)
-
-function printPerson(person: {name: string}) {
-    console.log(person.name)
-}
-
-printPerson({ name: "Kai" })
-printPerson({ name: "Edward Elric", age: 19 }) // this will not work BUT
-
-const person = { name: "Edward Elric", age: 19 }
-printPerson(person) // this will work
+printNameAndAge("sdf", 28)
