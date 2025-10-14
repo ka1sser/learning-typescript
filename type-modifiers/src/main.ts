cd3a839ca0de7d3baf0e74d0b7492415b6a4b96a
ka1sser <serenokaisser.dev@gmail.com>
2025-10-15 06:44:23 +0800

    type modifier: readonly

--- type-modifiers/src/main.ts
+++ type-modifiers/src/main.ts
@@ -1,12 +1,12 @@
 type Person = {
+  readonly id: number,
   name: string
   age: number
 }
 
-type PersonWithId = Person & { id: number }
+const person: Person = { id: 1, name: "Kai", age: 28 }
+// person.id = 2 # This will throw an error
 
-const person: PersonWithId = {
-  name: "Kai",
-  age: 28,
-  id: 1
-}
+type NumberArray = readonly number[]
+
+const nums: NumberArray = [1,2,3]
