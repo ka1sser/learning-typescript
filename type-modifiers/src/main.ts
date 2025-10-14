9ef81d1c6a130492690f17a808802f50652b3872
ka1sser <serenokaisser.dev@gmail.com>
2025-10-15 06:51:26 +0800

    type modifier: keyof

--- type-modifiers/src/main.ts
+++ type-modifiers/src/main.ts
@@ -1,12 +1,10 @@
 type Person = {
-  readonly id: number,
   name: string
   age: number
 }
 
-const person: Person = { id: 1, name: "Kai", age: 28 }
-// person.id = 2 # This will throw an error
+function getValue(key: keyof Person, person: Person) {
+  return person[key]
+}
 
-type NumberArray = readonly number[]
-
-const nums: NumberArray = [1,2,3]
+const age = getValue("age", {name: "Kai", age: 28})
